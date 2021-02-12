597055ea33f5f5aabf8a06d2370bd452701d58a5
aseat <73108756+aseat@users.noreply.github.com>
2021-02-12 20:24:30 +0900

    ユーザー情報

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: %i[update destroy]
 
-  
+  protected
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     passwords: 'users/passwords'
   }
+  get "users/show" => "users#show"
 
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
  
   root to: "concretes#index"
-  resources :users
+  
   resources :ways do
   resources :likes, only: [:create, :destroy]
   resources :waycomments, only: [:create, :destroy]
