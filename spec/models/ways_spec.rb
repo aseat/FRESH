23471fa61ef5024d9db34a2c5f5c872c256071c6
aseat <73108756+aseat@users.noreply.github.com>
2021-02-23 21:50:24 +0900

    試験問題単体テスト開始

---
+++ spec/models/ways_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe User, type: :model do
+  describe 'ユーザー新規登録' do
+    it 'nicknameが空では登録できない' do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.nickname = ''  # nicknameの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include "Nickname can't be blank"
+    end
+    it 'emailが空では登録できない' do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.email = ''  # emailの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include "Email can't be blank"
+    end
+  end
+end
