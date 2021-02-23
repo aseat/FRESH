9f9e5cf200b310b2375c11816146fd7f8ddf6730
aseat <73108756+aseat@users.noreply.github.com>
2021-02-23 19:30:20 +0900

    ユーザー編集機能実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,11 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: %i[update destroy]
 
-  protected
-
   def update_resource(resource, params)
     resource.update_without_password(params)
+  end
+
+  def after_update_path_for(resource)
+    root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
   get "users/show" => "users#show"
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
   
 
   devise_scope :user do
