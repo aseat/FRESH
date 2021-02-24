973672d4df0240611e6dee59fd9c00ed1e04f895
aseat <73108756+aseat@users.noreply.github.com>
2021-02-24 21:50:28 +0900

    ユーザー管理  テスト着手

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.modify do
+  factory :user do
+    nickname { Faker::Name.initials }
+    email { Faker::Internet.free_email }
+    password = Faker::Internet.password(min_length: 6)
+    password { password }
+    password_confirmation { password }
+    text { 'ああああ' }
+  end
+end
