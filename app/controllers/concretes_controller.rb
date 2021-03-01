72feef47a65966394197981ce37cd58c97ad37b7
aseat <73108756+aseat@users.noreply.github.com>
2021-03-01 21:52:29 +0900

    トップページ

--- app/controllers/concretes_controller.rb
+++ app/controllers/concretes_controller.rb
@@ -1,5 +1,8 @@
 class ConcretesController < ApplicationController
   def index
-    @ways = Way.all
+    @ways = Way.order(created_at: :desc).limit(1)
+    @words = Word.order(created_at: :desc).limit(1)
+    @questions = Question.order(created_at: :desc).limit(1)
+    @answers = Answer.order(created_at: :desc).limit(3)
   end
 end
