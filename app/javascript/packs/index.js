dd7c7d4e3dcdfcc35fd560a5fa39a48a9fcdebf1
aseat <lemnlsdrye231mk@gmail.com>
2021-06-15 13:35:03 +0900

    Vue.js

---
+++ app/javascript/packs/index.js
@@ -0,0 +1,12 @@
+mport 'babel-polyfill'
+import Vue from 'vue'
+
+// 作成したコンポーネントファイルをimportします
+import LikeButton from '../components/Like/LikeButton.vue'
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: '#like',
+    components: { LikeButton }
+  })
+})
