91e2404d019d6427098de2e796f74054c6b56a8f
aseat <lemnlsdrye231mk@gmail.com>
2021-07-19 20:58:59 +0900

    CSRF無効化

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Original
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
+    config.action_controller.allow_forgery_protection = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
