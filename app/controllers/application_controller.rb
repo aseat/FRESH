41a6cda3ba8179b82607016ca6229b7a4930450b
aseat <lemnlsdrye231mk@gmail.com>
2021-07-21 14:09:36 +0900

    rootpath

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,9 @@ class ApplicationController < ActionController::Base
     email = resource&.email || params[:user][:email].downcase
     redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。' if email == 'guest@example.com'
   end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path 
+  end
+
 end
