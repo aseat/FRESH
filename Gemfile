2658c813dc64c39ff22d9fa50cdadc665df44d74
aseat <lemnlsdrye231mk@gmail.com>
2021-07-22 10:17:50 +0900

    fresh

--- Gemfile
+++ Gemfile
@@ -67,7 +67,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
-gem 'bootstrap', '~> 5.0.0.beta1'
+gem 'bootstrap', '~> 5.0.1'
 gem 'devise'
 gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
---
+++ vendor/composer/installed.php
@@ -0,0 +1,40 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd63a6d19d9787c05841ff5a74e463b91caa5d063',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd63a6d19d9787c05841ff5a74e463b91caa5d063',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v5.0.1',
+      'version' => '5.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '58b1be927f43c779377e478df2d119f2ddf956ca',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v5.0.1',
+      ),
+    ),
+  ),
+);
